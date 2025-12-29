536a81b242831c2dedfeb2ddbafbe35d3bd02969
clara-cdp <clarianne.cdp@gmail.com>
2025-12-29 15:52:07 +0100

    exercise 2, nivell 2 complete with succeful tests

--- T8-N1-ex2/tests/CarSpeedSensorTest.php
+++ T8-N1-ex2/tests/CarSpeedSensorTest.php
@@ -11,10 +11,45 @@ class CarSpeedSensorTest extends TestCase
     {
 
         $sensor = new CarSpeedSensor();
-        $velocity = $sensor->getVelocity(20);
-        $this->assertEquals("Molt lent", $velocity);
+
+        $this->assertEquals("Molt lent", $sensor->getVelocity(3));
+        $this->assertEquals("Molt lent", $sensor->getVelocity(10));
+        $this->assertEquals("Molt lent", $sensor->getVelocity(26));
+    }
+
+
+    public function test_speedBetween31and60()
+    {
+        $sensor = new CarSpeedSensor();
+        $this->assertEquals("Velocitat adequada", $sensor->getVelocity(30));
+        $this->assertEquals("Velocitat adequada", $sensor->getVelocity(45));
+        $this->assertEquals("Velocitat adequada", $sensor->getVelocity(60));
+    }
+
+    public function test_speedBetween61and80()
+    {
+        $sensor = new CarSpeedSensor();
+        $this->assertEquals("Excés lleu", $sensor->getVelocity(61));
+        $this->assertEquals("Excés lleu", $sensor->getVelocity(72));
+        $this->assertEquals("Excés lleu", $sensor->getVelocity(79));
+    }
+
+    public function test_speedBetween81and100()
+    {
+        $sensor = new CarSpeedSensor();
+        $this->assertEquals("Excés moderat", $sensor->getVelocity(82));
+        $this->assertEquals("Excés moderat", $sensor->getVelocity(89));
+        $this->assertEquals("Excés moderat", $sensor->getVelocity(99));
+    }
+    public function test_speedOver101()
+    {
+        $sensor = new CarSpeedSensor();
+        $this->assertEquals("Excés greu", $sensor->getVelocity(102));
+        $this->assertEquals("Excés greu", $sensor->getVelocity(220));
+        $this->assertEquals("Excés greu", $sensor->getVelocity(150));
     }
 }
+
 
 
   /*  < 30 km/h: Molt lent

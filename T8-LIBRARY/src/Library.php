87a7b94fa27a4c0e24aa235fe51ade4f4267870e
clara-cdp <clarianne.cdp@gmail.com>
2026-02-12 10:49:05 +0100

    added setup method to testLibrary to reduce code

--- T8-LIBRARY/src/Library.php
+++ T8-LIBRARY/src/Library.php
@@ -6,23 +6,21 @@ class Library
 {
     private array $books = [];
 
-    public function addBook(Book $book): void
-    {
-        $this->books[] = $book;
-    }
-
     public function getBooks(): array
     {
         return $this->books;
     }
 
-    public function getBooksOver500pages(): array
+    //add and remove:
+    public function addBook(Book $newBook): void
     {
-        $booksList = array_values(array_filter($this->books, function ($book) {
-            return $book->getPages() > 500;
-        }));
+        foreach ($this->books as $book) {
+            if ($book->getIsbn() === $newBook->getIsbn()) {
+                throw new \Exception("El llibre amb aquest ISBN ja existeix.");
+            }
+        }
 
-        return $booksList;
+        $this->books[] = $newBook;
     }
 
     public function removeByISBN(string $isbn): void
@@ -34,5 +32,64 @@ class Library
                 break;
             }
         }
+    }
+
+    //find Books:
+
+    public function findByGenre(Genre $genre): array
+    {
+        $bookList = [];
+        foreach ($this->books as $book) {
+            if ($book->getGenre() === $genre) {
+                $bookList[] = $book;
+            }
+        }
+
+        return $bookList;
+    }
+
+    public function findByAuthor(string $author): array
+    {
+        $bookList = [];
+        foreach ($this->books as $book) {
+            if ($book->getAuthor() === $author) {
+                $bookList[] = $book;
+            }
+        }
+
+        return $bookList;
+    }
+
+    public function findByTitle(string $title): array
+    {
+        $bookList = [];
+        foreach ($this->books as $book) {
+            if ($book->getTitle() === $title) {
+                $bookList[] = $book;
+            }
+        }
+
+        return $bookList;
+    }
+
+    public function findByIsbn(string $isbn): ?Book
+    {
+        foreach ($this->books as $book) {
+            if ($book->getIsbn() === $isbn) {
+                return $book;
+            }
+        }
+        return null;
+    }
+
+
+    //check book has pages over500
+    public function getBooksOver500pages(): array
+    {
+        $booksList = array_values(array_filter($this->books, function ($book) {
+            return $book->getPagesNum() > 500;
+        }));
+
+        return $booksList;
     }
 }
--- T8-LIBRARY/tests/LibraryTest.php
+++ T8-LIBRARY/tests/LibraryTest.php
@@ -9,14 +9,39 @@ use LibraryApp\Genre;
 
 class LibraryTest extends TestCase
 {
-    //Afegeixin, esborrin i modifiquin un llibre de la llibreria.
+
+    private Library $library;
+
+    protected function setUp(): void
+    {
+        $this->library = new Library();
+        $this->library->addBook(new Book("Dune", "Herbert", "978-0801950773", Genre::SYFY, 600));
+        $this->library->addBook(new Book("The Hobbit", "Tolkien", "9780261102217", Genre::Fantasy, 310));
+        $this->library->addBook(new Book("The Shadow of the Wind", "Carlos Ruiz Zafón", '9788408043645', Genre::Paranormal, 576));
+        $this->library->addBook(new Book("La cúpula", "Stephen King", "9788401337635", Genre::SYFY, 500)); //large book limit
+        $this->library->addBook(new Book("La nit de l'escola", "Karl Ove Knausgård", "9780525562078", Genre::Paranormal, 501)); //book over 500 pgs
+    }
+
+    //Afegeixin, esborrin (i modifiquin) un llibre de la llibreria.
     public function testAddBookToLibrary(): void
     {
-        $library = new Library();
-        $book = new Book("The Hobbit", "J.R.R. Tolkien", "9780261102217", Genre::Fantasy, 310);
+        $newBook = new Book("1984", "George Orwell", "9780451524935", Genre::Dystopian, 328);
 
-        $library->addBook($book);
-        $this->assertCount(1, $library->getBooks());
+        $this->library->addBook($newBook);
+        $this->assertCount(6, $this->library->getBooks());
+    }
+
+    //-> NEW TEST. what happens i add a bokk that alreay exists?? 
+    public function testAddBookDuplicateBookException(): void
+    {
+        $this->expectException(\Exception::class);
+        $this->expectExceptionMessage("El llibre amb aquest ISBN ja existeix.");
+
+        $book1 = new Book("Cròniques de la veritat oculta", "Pere Calders", "9788472221659", Genre::Story, 192);
+        $book2 = new Book("The Hobbit", "Tolkien", "9780261102217", Genre::Fantasy, 310); //already exists
+
+        $this->library->addBook($book1);
+        $this->library->addBook($book2); // exception case
     }
 
     public function testLibraryStartsEmpty(): void
@@ -25,31 +50,62 @@ class LibraryTest extends TestCase
         $this->assertCount(0, $library->getBooks());
     }
 
-
     public function testRemoveBookByISBN(): void
     {
-        $library = new Library();
-        $isbn = "978-8408043645";
-        $book = new Book("The Shadow of the Wind", "Carlos Ruiz Zafón", $isbn, Genre::Paranormal, 576);
+        $isbn = "9788408043645";
+        $this->library->removeByISBN($isbn);
 
-        $library->addBook($book);
-
-        $library->removeByISBN($isbn);
-
-        $this->assertCount(0, $library->getBooks(), "El llibre no s'ha esborrat correctament.");
+        $this->assertCount(4, $this->library->getBooks(), "El llibre no s'ha esborrat correctament.");
     }
+
     //Permetin consultar llibres per títol, gènere, ISBN o autor.
+    public function testFindByTitle(): void
+    {
+        $results = $this->library->findByTitle("Dune");
+
+        $this->assertCount(1, $results);
+        $this->assertEquals("Dune", $results[0]->getTitle());
+    }
+
+    public function testFindByGenre(): void
+    {
+        $genre = Genre::SYFY;
+        $results = $this->library->findByGenre($genre);
+
+        $this->assertCount(2, $results);
+        $this->assertEquals("Dune", $results[0]->getTitle());
+    }
+
+    public function testFindByISBN(): void
+    {
+        $ISBN = "9780261102217";
+        $result = $this->library->findByISBN($ISBN);
+
+        $this->assertInstanceOf(Book::class, $result);
+        $this->assertEquals("The Hobbit", $result->getTitle());
+    }
+
+    public function testFindByAuthor(): void
+    {
+        $author = "Tolkien";
+        $results = $this->library->findByAuthor($author);
+
+        $this->assertCount(1, $results);
+        $this->assertEquals("The Hobbit", $results[0]->getTitle());
+    }
+
 
     //Retornar llibres grans (més de 500 pàgines).
     public function testFilterBooksOver500pages(): void
     {
-        $library = new Library();
-        $library->addBook(new Book("Limit Book", "Author", "1", Genre::Story, 500)); // Should NOT be in results
-        $library->addBook(new Book("Large Book", "Author", "2", Genre::SYFY, 501)); // Should BE in results
+        $largeBooks = $this->library->getBooksOver500pages();
 
-        $largeBooks = $library->getBooksOver500pages();
+        $this->assertCount(3, $largeBooks);
 
-        $this->assertCount(1, $largeBooks);
-        $this->assertEquals("Large Book", $largeBooks[0]->getTitle());
+        $titles = array_map(fn($book) => $book->getTitle(), $largeBooks);
+
+        $this->assertContains("La nit de l'escola", $titles);
+        $this->assertContains("Dune", $titles);
+        $this->assertContains("The Shadow of the Wind", $titles);
     }
 }
